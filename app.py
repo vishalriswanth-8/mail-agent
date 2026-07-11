f69d1829d6d390727a1f4f73dcb95db01dd097f2
vishalriswanth-8 <mr.riswanth@gmail.com>
2026-07-11 10:52:18 +0530

    Add Access-Control-Allow-Private-Network header to resolve Chrome PNA loopback blocks

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ def handle_options():
         headers["Access-Control-Allow-Origin"] = "*"
         headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
         headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-Requested-With"
+        headers["Access-Control-Allow-Private-Network"] = "true"
         return response
 
 @app.after_request
@@ -37,6 +38,7 @@ def add_cors_headers(response):
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-Requested-With"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Private-Network"] = "true"
     return response
 
 
